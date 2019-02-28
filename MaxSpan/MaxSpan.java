01e8b1936c79d822af307c82546546255b543b79
Jason Wu <jasonwu_8@hotmail.com>
2019-02-27 19:12:12 -0800

    forgot the header for this problem

--- MaxSpan/MaxSpan.java
+++ MaxSpan/MaxSpan.java
@@ -1,3 +1,10 @@
+/*
+ * https://codingbat.com/prob/p189576
+ * Consider the leftmost and righmost appearances of some value in an array. We'll say that the "span" is the number of elements between the two inclusive. A single value has a span of 1. Returns the largest span found in the given array. (Efficiency is not a priority.)
+ * maxSpan([1, 2, 1, 1, 3]) → 4
+ * maxSpan([1, 4, 2, 1, 4, 1, 4]) → 6
+ * maxSpan([1, 4, 2, 1, 4, 4, 4]) → 6
+ */
 public int maxSpan(int[] nums) {
     HashMap<Integer, Integer> positionMap = new HashMap<>();
     int maxSpan = 0;
