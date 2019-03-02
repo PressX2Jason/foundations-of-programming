9876dab1afcafedefb34b8b77f17d475473d1cbe
Jason Wu <jasonwu_8@hotmail.com>
2019-03-01 22:41:39 -0800

    ran through formatter

--- hangman/hangman_lexicon.py
+++ hangman/hangman_lexicon.py
@@ -1,19 +1,19 @@
 class HangmanLexicon():
     def __init__(self):
         self.words = {
-            0 : 'BUOY',
-            1 : 'COMPUTER',
-            2 : 'CONNOISSEUR',
-            3 : 'DEHYDRATE',
-            4 : 'FUZZY',
-            5 : 'HUBBUB',
-            6 : 'KEYHOLE',
-            7 : 'QUAGMIRE',
-            8 : 'SLITHER',
-            9 : 'ZIRCON',
+            0: 'BUOY',
+            1: 'COMPUTER',
+            2: 'CONNOISSEUR',
+            3: 'DEHYDRATE',
+            4: 'FUZZY',
+            5: 'HUBBUB',
+            6: 'KEYHOLE',
+            7: 'QUAGMIRE',
+            8: 'SLITHER',
+            9: 'ZIRCON',
         }
         self.wordCount = len(self.words)
-    
+
     def get_word_count(self):
         return self.wordCount
 
