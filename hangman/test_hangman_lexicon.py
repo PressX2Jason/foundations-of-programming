fd83982d1b829dd9ce9d9a40f1b5792dc341febc
Jason Wu <jasonwu_8@hotmail.com>
2019-03-02 20:02:42 -0800

    simplified the test

--- hangman/test_hangman_lexicon.py
+++ hangman/test_hangman_lexicon.py
@@ -3,20 +3,37 @@ from hangman_lexicon import HangmanLexicon
 
 lexicon = HangmanLexicon()
 
-@pytest.mark.parametrize('index, word', [
-    (0 , 'BUOY'),
-    (1 , 'COMPUTER'),
-    (2 , 'CONNOISSEUR'),
-    (3 , 'DEHYDRATE'),
-    (4 , 'FUZZY'),
-    (5 , 'HUBBUB'),
-    (6 , 'KEYHOLE'),
-    (7 , 'QUAGMIRE'),
-    (8 , 'SLITHER'),
-    (9 , 'ZIRCON'),
-])
+seedWords = [
+    'BUOY',
+    'COMPUTER',
+    'CONNOISSEUR',
+    'DEHYDRATE',
+    'FUZZY',
+    'HUBBUB',
+    'KEYHOLE',
+    'QUAGMIRE',
+    'SLITHER',
+    'ZIRCON'
+]
+
+
+@pytest.mark.parametrize('index, word',
+                         [(index, word)
+                          for index, word in enumerate(seedWords)]
+                         )
 def test_lexicon_get_word(index, word):
     assert lexicon.get_word(index) == word
 
+
 def test_lexicon_get_word_count():
     assert lexicon.get_word_count() == 10
+
+
+def test_get_random_word():
+    word = lexicon.get_random_word()
+    assert word in seedWords
+
+
+def test_get_word_out_of_bounds():
+    with pytest.raises(IndexError):
+        lexicon.get_word(99)
