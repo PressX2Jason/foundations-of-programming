d638876dd303ed5c87b5d9e6eabc3b6a54f177ba
Jason Wu <jasonwu_8@hotmail.com>
2019-03-02 19:49:52 -0800

    tests cover 100% of hangman.py now

--- hangman/test_hangman.py
+++ hangman/test_hangman.py
@@ -1,6 +1,7 @@
 import pytest
 from hangman import Hangman
 
+
 class MockLexicon():
     def __init__(self):
         self.words = {
@@ -20,6 +21,53 @@ class MockLexicon():
         return self.words[index]
 
 
+def test_get_secret_word():
+    game = Hangman(MockLexicon())
+    assert game.get_secret_word() == 'TESTING'
+
+def test_game_over_by_out_of_guesses():
+    game = Hangman(MockLexicon())
+    for _ in range(8):
+        game.guess_letter('X')
+    assert game.game_over() == True
+
+def test_game_over_by_correctly_guessed():
+    game = Hangman(MockLexicon())
+    game.guess_letter('T')
+    game.guess_letter('E')
+    game.guess_letter('S')
+    game.guess_letter('I')
+    game.guess_letter('N')
+    game.guess_letter('G')
+    assert game.game_over() == True
+
+
+def test_secret_guessed():
+    game = Hangman(MockLexicon())
+    game.guess_letter('T')
+    assert game.secret_guessed() == False
+    game.guess_letter('E')
+    assert game.secret_guessed() == False
+    game.guess_letter('S')
+    assert game.secret_guessed() == False
+    game.guess_letter('I')
+    assert game.secret_guessed() == False
+    game.guess_letter('N')
+    assert game.secret_guessed() == False
+    game.guess_letter('G')
+    assert game.secret_guessed() == True
+
+
+def test_out_of_guesses():
+    game = Hangman(MockLexicon())
+    for i in range(8):
+        game.guess_letter('X')
+        if i == 7:
+            assert game.out_of_guesses() == True
+        else:
+            assert game.out_of_guesses() == False
+
+
 @pytest.mark.parametrize('guess, current_guess', [
     ('', '-------'),
     ('T', 'T--T---'),
@@ -35,26 +83,6 @@ def test_get_current_guess(guess, current_guess):
     game.guess_letter(guess)
     assert game.get_current_guess() == current_guess
 
-def test_get_secret_word():
-    game = Hangman(MockLexicon())
-    assert game.get_secret_word() == 'TESTING'
-
-def test_secret_guessed():
-    game = Hangman(MockLexicon())
-    game.guess_letter('T')
-    game.guess_letter('E')
-    game.guess_letter('S')
-    game.guess_letter('I')
-    game.guess_letter('N')
-    game.guess_letter('G')
-    assert game.secret_guessed
-
-def test_out_of_guesses():
-    game = Hangman(MockLexicon())
-    for _ in range(8):
-        game.guess_letter('X')
-        assert not game.out_of_guesses()
-    assert game.out_of_guesses()
 
 @pytest.mark.parametrize('guess, result', [
     ('', False),
